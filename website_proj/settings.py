dfba7019df2713414a5cf4cb1738a5b07cdbc2ef
guilhermegouw <guilherme.gouw@gmail.com>
2022-02-20 22:16:36 -0300

    chapter 10

--- website_proj/settings.py
+++ website_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_09e(4b*v2^z0sg^onfloo#qpiyld8td^9pvyv6n0_+y49)^50'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
